3a879bc5bd186c1908e40cc825da1ea94dcbcb8c
Егор <@KITTLE>
2022-11-13 12:33:50 +0300

    Add project files.

---
+++ HomeW0rkForm2_2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeW0rkForm2_2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Shown += StartGame;
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            DialogResult result;
+            int numDialog = 1;
+            while (true)
+            {
+                result = MessageBox.Show($"{new Random().Next(1, 2000)}", "Вы загадали число", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    MessageBox.Show($"Количество запросов {numDialog}", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    numDialog = 0;
+                    result = MessageBox.Show($"Хотите продолжить?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No) this.Close();
+                }
+                numDialog++;
+            }
+        }
+    }
+}
